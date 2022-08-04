16619926a8ad5c3a6b948e23aefd050fa6680e4d
Daniel Castro <johndaniel.castro@globant.com>
2022-08-04 11:21:48 -0500

    Added build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from '@rollup/plugin-babel';
 import commonjs from "@rollup/plugin-commonjs";
 import copy from 'rollup-plugin-copy'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
@@ -18,13 +17,9 @@ export default [
         },
         plugins: [
             peerDepsExternal(),
-            resolve({ resolveOnly: module => {
-                console.log(module);
-                return !module.includes('.scss')
-            } }),
+            resolve(),
             babel({ presets: ["@babel/preset-react"] }),
             commonjs(),
-            postcss(),
             terser(),
             copy({ targets: [
                 { src: 'src/*.scss', dest: 'dist' },
