586f73c8eaf4175d267b6217a495f6d9e0cd579d
Daniel Castro <johndaniel.castro@globant.com>
2022-08-23 10:08:30 -0500

    Reset packages versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from "@rollup/plugin-commonjs";
 import copy from 'rollup-plugin-copy'
+import del from 'rollup-plugin-delete'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-
-const packageJson = require("./packages/button/package.json");
 
 export default [
     {
         input: "src/index.js",
         output: {
-            file: packageJson.main,
+            file: "dist/main.js",
             format: "cjs",
             sourcemap: true,
         },
         plugins: [
+            del({ targets: 'dist/*' }),
             peerDepsExternal(),
             resolve(),
             babel({ presets: ["@babel/preset-react"] }),
